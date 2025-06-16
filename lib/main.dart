3b1cbb4cabeaccef2204d5a37bf519db792a57d6
mk369 <klilmohammed9@gmail.com>
2025-06-16 16:26:06 +0300

    refactor: make the sonarQube workflow to run again

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'firebase_options.dart';
 import 'shared_layers/localization/generated/app_localizations.dart';
 
 GlobalKey<NavigatorState> globalNavigatorKey = GlobalKey<NavigatorState>();
+// just sonarQube workflow to run again
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 

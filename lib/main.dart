7e9c34445cda3c283198ba207c37f3b6f818db54
mk369 <klilmohammed9@gmail.com>
2025-06-19 18:10:10 +0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class MyApp extends StatelessWidget {
             //   return GenerateRoute.onGenerateInitialRoutes(
             //     initialRoute: initialRoute,
             //     loginInfo: null,
-            //   );
+            //   );ref
             // },
           ),
         );
--- lib/modules/authentication/ui/complete_register/widgets/select_goal_screen.dart
+++ lib/modules/authentication/ui/complete_register/widgets/select_goal_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:super_fitness/core/di/injectable_initializer.dart';

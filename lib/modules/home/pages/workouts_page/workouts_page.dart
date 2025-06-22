ab8d9386b3944fb930f517aba796719f849df35a
7oda <7oda@gmail.com>
2025-06-22 16:54:29 +0300

    feat(workouts): finish ui .

--- lib/modules/home/pages/workouts_page/workouts_page.dart
+++ lib/modules/home/pages/workouts_page/workouts_page.dart
@@ -1,44 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:super_fitness/core/bases/base_statless_widget.dart';
-import 'package:super_fitness/core/widgets/custom_item_container.dart';
-import 'package:super_fitness/core/widgets/custom_network_cached_image.dart';
+import 'package:super_fitness/core/bases/base_stateful_widget_state.dart';
 
-class WorkoutsPage extends BaseStatelessWidget {
+import '../../../../core/widgets/custom_bottom_tab_bar.dart';
+import '../../../../core/widgets/custom_item_container.dart';
+
+class WorkoutsPage extends StatefulWidget {
   const WorkoutsPage({super.key});
 
   @override
-  Widget customBuild(BuildContext context, inherit) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        SizedBox(height: inherit.screenHeight * 0.1,),
-        Text(
-          "CustomNetwork\nCachedImage",
-          textAlign: TextAlign.center,
-          style: inherit.theme.textTheme.titleLarge!.copyWith(
-            fontWeight: FontWeight.w800,
+  State<WorkoutsPage> createState() => _WorkoutsPageState();
+}
+
+class _WorkoutsPageState extends BaseStatefulWidgetState<WorkoutsPage> {
+  int currentTabIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 10,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Workouts'),
+          centerTitle: true,
+          bottom: CustomBottomTabBar(
+            isScrollable: true,
+            onTabClick: (index) {
+              setState(() {
+                currentTabIndex = index;
+              });
+            },
+            tabs: const [
+              Tab(child: Text("Full Body")),
+              Tab(child: Text("Chest")),
+              Tab(child: Text("Arm")),
+              Tab(child: Text("Arm")),
+              Tab(child: Text("Arm")),
+              Tab(child: Text("Full Body")),
+              Tab(child: Text("Chest")),
+              Tab(child: Text("Arm")),
+              Tab(child: Text("Arm")),
+              Tab(child: Text("Arm")),
+            ],
           ),
         ),
-        CustomNetworkCachedImage(
-          imageUrl:
-          "https://www.themealdb.com/images/media/meals/2dsltq1560461468.jpg",
-          width: inherit.screenWidth * 0.6,
-          height: inherit.screenHeight * 0.26,
-        ),
-        SizedBox(height: inherit.screenHeight * 0.02),
-        Text(
-          "CustomItemContainer",
-          style: inherit.theme.textTheme.titleLarge!.copyWith(
-            fontWeight: FontWeight.w800,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 24),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 18,
+              mainAxisSpacing: 18,
+            ),
+            itemBuilder:
+                (context, index) => CustomItemContainer(
+                  imageUrl: "https://iili.io/33p7y9p.png",
+                  width: screenWidth * 0.6,
+                  height: screenHeight * 0.26,
+                  title: "Chest Exercise".replaceFirst(" ", "\n"),
+                ),
+
+            itemCount: 10,
           ),
         ),
-        CustomItemContainer(
-          imageUrl: "https://iili.io/33p7y9p.png",
-          width: inherit.screenWidth * 0.6,
-          height: inherit.screenHeight * 0.26,
-          title: "Chest Exercise".replaceFirst(" ", "\n"),
-        ),
-      ],
+      ),
     );
   }
 }

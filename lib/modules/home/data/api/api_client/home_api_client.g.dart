49906006acddd0dad5b7344d98a0b88905977160
7oda <7oda@gmail.com>
2025-06-26 17:55:11 +0300

    feat(food-details): merge develop into branch .

--- lib/modules/home/data/api/api_client/home_api_client.g.dart
+++ lib/modules/home/data/api/api_client/home_api_client.g.dart
@@ -9,13 +9,13 @@ part of 'home_api_client.dart';
 // ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
 
 class _HomeApiClient implements HomeApiClient {
-  _HomeApiClient(this._dio, );
+  _HomeApiClient(this._dio, {this.baseUrl, this.errorLogger});
 
   final Dio _dio;
 
   String? baseUrl;
 
-   ParseErrorLogger? errorLogger;
+  final ParseErrorLogger? errorLogger;
 
   @override
   Future<RandomExercisesResponseDto> getRandomExercises({
--- lib/shared_layers/localization/generated/app_localizations_ar.dart
+++ lib/shared_layers/localization/generated/app_localizations_ar.dart
@@ -48,8 +48,7 @@ class AppLocalizationsAr extends AppLocalizations {
   String get userNameLength => 'يجب أن يكون اسم المستخدم بين 3 و16 حرفًا.';
 
   @override
-  String get userNameRules =>
-      'يمكن أن يحتوي اسم المستخدم على أحرف وأرقام وشرطات سفلية فقط.';
+  String get userNameRules => 'يمكن أن يحتوي اسم المستخدم على أحرف وأرقام وشرطات سفلية فقط.';
 
   @override
   String get pleaseEnterFirstName => 'يرجى إدخال الاسم الأول.';
@@ -70,15 +69,13 @@ class AppLocalizationsAr extends AppLocalizations {
   String get pleaseEnterPhoneNumber => 'يرجى إدخال رقم هاتفك.';
 
   @override
-  String get phoneNumberRules =>
-      'يجب أن يبدأ الرقم بأحد البادئات التالية:\n010، 011، 012، أو 015\nويتبعها 8 أرقام.';
+  String get phoneNumberRules => 'يجب أن يبدأ الرقم بأحد البادئات التالية:\n010، 011، 012، أو 015\nويتبعها 8 أرقام.';
 
   @override
   String get pleaseEnterPassword => 'يرجى إدخال كلمة المرور.';
 
   @override
-  String get passwordLength =>
-      'يجب أن تكون كلمة المرور مكونة من 8 أحرف على الأقل.';
+  String get passwordLength => 'يجب أن تكون كلمة المرور مكونة من 8 أحرف على الأقل.';
 
   @override
   String get uppercaseRulePassword => 'حرف كبير واحد على الأقل.';
@@ -90,15 +87,13 @@ class AppLocalizationsAr extends AppLocalizations {
   String get digitRulePassword => 'يجب أن تحتوي على رقم واحد على الأقل.';
 
   @override
-  String get specialCharactersRulePassword =>
-      'يجب تضمين رمز خاص واحد على الأقل (مثال: #?!@\$%^&*-).';
+  String get specialCharactersRulePassword => 'يجب تضمين رمز خاص واحد على الأقل (مثال: #?!@\$%^&*-).';
 
   @override
   String get pleaseConfirmPassword => 'يرجى تأكيد كلمة المرور.';
 
   @override
-  String get namesLengthRule =>
-      'يجب أن تكون الأسماء مكونة من 3 أحرف على الأقل.';
+  String get namesLengthRule => 'يجب أن تكون الأسماء مكونة من 3 أحرف على الأقل.';
 
   @override
   String get noMatch => 'غير متطابق!';
@@ -125,8 +120,7 @@ class AppLocalizationsAr extends AppLocalizations {
   String get removedImage => 'تمت إزالة الصورة';
 
   @override
-  String get forgetPasswordDescription =>
-      'يرجى إدخال بريدك الإلكتروني المرتبط بحسابك';
+  String get forgetPasswordDescription => 'يرجى إدخال بريدك الإلكتروني المرتبط بحسابك';
 
   @override
   String get confirm => 'تأكيد';
@@ -138,8 +132,7 @@ class AppLocalizationsAr extends AppLocalizations {
   String get otpScreenTitle => 'التحقق من البريد الإلكتروني';
 
   @override
-  String get otpScreenDescription =>
-      'يرجى إدخال الرمز الذي تم إرساله إلى بريدك الإلكتروني';
+  String get otpScreenDescription => 'يرجى إدخال الرمز الذي تم إرساله إلى بريدك الإلكتروني';
 
   @override
   String get didNotReceiveOtp => 'لم تستلم الرمز؟';
@@ -154,8 +147,7 @@ class AppLocalizationsAr extends AppLocalizations {
   String get codeNotValid => 'الرمز غير صالح';
 
   @override
-  String get resetPasswordScreenDescription =>
-      'يجب ألا تكون كلمة المرور فارغة ويجب أن تحتوي على 6 أحرف على الأقل مع حرف كبير ورقم واحد على الأقل';
+  String get resetPasswordScreenDescription => 'يجب ألا تكون كلمة المرور فارغة ويجب أن تحتوي على 6 أحرف على الأقل مع حرف كبير ورقم واحد على الأقل';
 
   @override
   String get resetPasswordScreenTitle => 'إعادة تعيين كلمة المرور';
@@ -209,12 +201,10 @@ class AppLocalizationsAr extends AppLocalizations {
   String get doItNow => 'افعلها الآن';
 
   @override
-  String get loremIpsumDolorSitAmetConsectetureuUrna =>
-      'لوريم إيبسوم دولور سيت أميت. يو أورنا';
+  String get loremIpsumDolorSitAmetConsectetureuUrna => 'لوريم إيبسوم دولور سيت أميت. يو أورنا';
 
   @override
-  String get utGravidaQuisIdPretiumPurusMaurisMassa =>
-      'يوت جرافيدا كويز إد بريتيوم بورس. موريس ماسا';
+  String get utGravidaQuisIdPretiumPurusMaurisMassa => 'يوت جرافيدا كويز إد بريتيوم بورس. موريس ماسا';
 
   @override
   String get pleaseEnterValidRecipient => 'يرجى إدخال مستلم صالح';
@@ -295,8 +285,7 @@ class AppLocalizationsAr extends AppLocalizations {
   String get whatIsYourWeight => 'ما هو وزنك؟';
 
   @override
-  String get thisHelpsUsCreateYourPersonalizedPlan =>
-      'يساعدنا ذلك على إنشاء خطة مخصصة لك';
+  String get thisHelpsUsCreateYourPersonalizedPlan => 'يساعدنا ذلك على إنشاء خطة مخصصة لك';
 
   @override
   String get whatIsYourHeight => 'ما هو طولك؟';
@@ -395,13 +384,25 @@ class AppLocalizationsAr extends AppLocalizations {
   String get successEmailVerified => 'تم التحقق من البريد الإلكتروني بنجاح';
 
   @override
-  String get foodRecommendation => 'توصيات غذائية';
+  String get foodRecommendation => 'تفضيلات الطعام';
 
   @override
-  String get noVideoProvide => 'لا يوجد فيديو متاح';
+  String get noVideoProvide => 'الفديو غير موجود';
 
   @override
-  String get noFoodRecommendation => 'لم يتم العثور على توصيات غذائية';
+  String get noFoodRecommendation => 'لايوجد تفضيلات طعام';
+
+  @override
+  String get ingredients => 'المكونات';
+
+  @override
+  String get recommendation => 'التوصية';
+
+  @override
+  String get energy => 'الطاقة';
+
+  @override
+  String get energyValue => '١٠٠ ك';
 
   @override
   String get noWorkoutsAvailable => 'لا توجد تمارين متاحة';

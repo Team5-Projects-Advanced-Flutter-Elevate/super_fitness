0d40f4926e17091a18930279af9a39270c87290d
7ouda200 <mmg2017mg@gmail.com>
2025-06-29 01:02:30 +0200

    feat(fixLoginBug)

--- lib/core/themes/app_themes.dart
+++ lib/core/themes/app_themes.dart
@@ -214,7 +214,7 @@ abstract class AppThemes {
         color: AppColors.mainColorDark,
       ),
     ),
-    tabBarTheme: TabBarTheme(
+    tabBarTheme: TabBarThemeData(
       dividerColor: AppColors.transparent,
       dividerHeight: 0,
       overlayColor: WidgetStateColor.transparent,

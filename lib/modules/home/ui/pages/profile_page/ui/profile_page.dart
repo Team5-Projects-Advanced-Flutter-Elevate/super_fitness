87a3f4707a58c1a316fa19178771725a3d694d1c
7oda <7oda@gmail.com>
2025-07-03 15:56:04 +0300

    feat(profile): fix switch bug and make profile loading only first time .

--- lib/modules/home/ui/pages/profile_page/ui/profile_page.dart
+++ lib/modules/home/ui/pages/profile_page/ui/profile_page.dart
@@ -15,11 +15,12 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends BaseStatefulWidgetState<ProfilePage> {
-  ProfileCubit profileCubit = getIt<ProfileCubit>();
+  late ProfileCubit profileCubit;
 
   @override
   void initState() {
     super.initState();
+    profileCubit = BlocProvider.of<ProfileCubit>(context);
     profileCubit.doIntent(GetProfileDataIntent());
   }
 
@@ -29,21 +30,18 @@ class _ProfilePageState extends BaseStatefulWidgetState<ProfilePage> {
       appBar: AppBar(title: Text(appLocalizations.profile), centerTitle: true),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-        child: BlocProvider(
-          create: (context) => profileCubit,
-          child: BlocBuilder<ProfileCubit, ProfileState>(
-            builder: (context, state) {
-              switch (state.status) {
-                case ProfileStatus.initial:
-                case ProfileStatus.loading:
-                  return const LoadingStateWidget();
-                case ProfileStatus.success:
-                  return SuccessState(state: state, cubit: profileCubit);
-                case ProfileStatus.error:
-                  return ErrorStateWidget(error: state.getProfileError!);
-              }
-            },
-          ),
+        child: BlocBuilder<ProfileCubit, ProfileState>(
+          builder: (context, state) {
+            switch (state.status) {
+              case ProfileStatus.initial:
+              case ProfileStatus.loading:
+                return const LoadingStateWidget();
+              case ProfileStatus.success:
+                return SuccessState(state: state, cubit: profileCubit);
+              case ProfileStatus.error:
+                return ErrorStateWidget(error: state.getProfileError!);
+            }
+          },
         ),
       ),
     );

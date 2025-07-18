dcda0f904b9784bca5c80c96620a7e8cab14940e
mohamed-sherif <mohamedsherifkamal19@gmail.com>
2025-07-19 02:07:52 +0300

    feat(readme ):fix linter

--- lib/modules/authentication/ui/complete_register/widgets/select_age_screen.dart
+++ lib/modules/authentication/ui/complete_register/widgets/select_age_screen.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
-import 'dart:ui';
 
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:numberpicker/numberpicker.dart';
